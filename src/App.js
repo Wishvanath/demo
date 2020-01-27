7e201b683be482c844b053a934b1f8635b1fdeb0
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-27 10:59:44 +0530

    fixed Route Issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from './component/home';
+import About from './component/about';
+import ContactUs from './component/contact-us';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        This is Modified app Component
-      </div>
+      <BrowserRouter>
+      <Switch>
+          <Route exact path ="/" component ={Home} />
+          <Route exact path ="/about" component ={About} />
+          <Route exact path ="/contact" component ={ContactUs} />
+
+      </Switch>
+    </BrowserRouter>
     )
   }
 }
