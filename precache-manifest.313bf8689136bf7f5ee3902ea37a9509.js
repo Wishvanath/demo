a5a37a89da85774f8de9417147c4933947d79bfe
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-27 10:21:07 +0530

    Updates

---
+++ precache-manifest.313bf8689136bf7f5ee3902ea37a9509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b05453fe68bf5df5d8c4fc2889a35f2",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "7440a175f1916ed37eb3",
+    "url": "/demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b9b079c665626bd11c5",
+    "url": "/demo/static/js/2.f9f415c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo/static/js/2.f9f415c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7440a175f1916ed37eb3",
+    "url": "/demo/static/js/main.60bc1bfb.chunk.js"
+  },
+  {
+    "revision": "9c1467caca986250cf4b",
+    "url": "/demo/static/js/runtime-main.24fd0856.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
